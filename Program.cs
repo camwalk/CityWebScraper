502798662a861716be4571c81b6ccf15cace1cce
camwalk <camwalkuk@gmail.com>
2020-01-24 15:39:02 +0000

    intial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IronWebScraper;
+
+namespace CityWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scraper = new CityScraper();
+            scraper.Start();
+        }
+    }
+
+    class CityScraper : WebScraper
+    {
+        public override void Init()
+        {
+            
+        }
+    }
+}

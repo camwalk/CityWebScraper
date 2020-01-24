50a9fa04884984752f591311ca1650710e2a5802
camwalk <camwalkuk@gmail.com>
2020-01-24 16:37:55 +0000

    complete code

--- Program.cs
+++ Program.cs
@@ -16,7 +16,21 @@ namespace CityWebScraper
     {
         public override void Init()
         {
-            
+            this.LoggingLevel = WebScraper.LogLevel.All;
+            this.Request("https://www.cityofglasgowcollege.ac.uk/course-search", Parse);
         }
+        public override void Parse(Response response)
+        {
+            foreach (var title_link in response.Css("a.panel-course-results__course"))
+            {
+                string strTitle = title_link.TextContentClean;
+                Scrape(new ScrapedData() { { "Title", strTitle } });
+            }
+            if (response.CssExists("li.pager-next > a[href]"))
+            {
+                var next_page = response.Css("li.pager-next > a[href]")[0].Attributes["href"];
+                this.Request(next_page, Parse);
+            }
+        }   
     }
 }
